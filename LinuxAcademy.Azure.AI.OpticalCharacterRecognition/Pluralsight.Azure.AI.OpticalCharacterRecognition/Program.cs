0f564d4be0e49a68dd68679a6d2e2f23ed962cee
mattuLA <matthew-ulasien@pluralsight.com>
2025-11-03 11:54:30 -0600

    updates

--- LinuxAcademy.Azure.AI.OpticalCharacterRecognition/Pluralsight.Azure.AI.OpticalCharacterRecognition/Program.cs
+++ LinuxAcademy.Azure.AI.OpticalCharacterRecognition/Pluralsight.Azure.AI.OpticalCharacterRecognition/Program.cs
@@ -8,7 +8,7 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Pluralsight.Azure.AI.OpticalCharacterRecognition
+namespace LinuxAcademy.Azure.AI.OpticalCharacterRecognition
 {
     class Program
     {
--- LinuxAcademy.Azure.AI.OpticalCharacterRecognition/Pluralsight.Azure.AI.OpticalCharacterRecognition/support/ApiKeyServiceClientCredentials.cs
+++ LinuxAcademy.Azure.AI.OpticalCharacterRecognition/Pluralsight.Azure.AI.OpticalCharacterRecognition/support/ApiKeyServiceClientCredentials.cs
@@ -7,7 +7,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Pluralsight.Azure.AI.OpticalCharacterRecognition.support
+namespace LinuxAcademy.Azure.AI.OpticalCharacterRecognition.support
 {
     public class ApiKeyServiceClientCredentials : ServiceClientCredentials
     {
